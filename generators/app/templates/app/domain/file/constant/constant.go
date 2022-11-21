054f0b4c1a98e868f2f369da17ced73fe4d63928
thinh.nguyen <thinh.nguyen@rayyone.com>
2022-11-21 11:14:57 +0700

    add file upload

---
+++ generators/app/templates/app/domain/file/constant/constant.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright Rayyone Ltd. 2022. All Rights Reserved
+ * Node module: @rayyone/go-generator
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+package constant
+
+const (
+	Image = "image"
+	UserProfile = "user_profile"
+	JobImage = "job_image"
+)
